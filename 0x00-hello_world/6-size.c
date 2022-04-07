ca481dd2f23c28845b39fbe1ef0179535e61fe59
awinbern <awinpangakudbillabernice4@gmail.com>
2022-04-07 16:36:19 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,6 @@
 */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
 printf("size of a char: % 1u byte(s)\n", (unsigned long)sizeof(d));
 printf("size of an int: % 1u byte(s)\n", (unsigned long)sizeof(a));
 printf("size of a long int: % 1u byte(s)\n", (unsigned long)sizeoff(b));
