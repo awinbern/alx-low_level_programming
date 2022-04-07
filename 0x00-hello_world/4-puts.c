357671707d6470b37899fc60ba7bc1fcaa48d6ec
awinbern <awinpangakudbillabernice4@gmail.com>
2022-04-07 11:53:41 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C main function
+ * Return: return function
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
