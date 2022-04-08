e90346a1c4d4a71d9d54435e0e0f32cae41b3628
awinbern <awinpangakudbillabernice4@gmail.com>
2022-04-08 15:14:45 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - defines function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'a'; j <= 'f'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
