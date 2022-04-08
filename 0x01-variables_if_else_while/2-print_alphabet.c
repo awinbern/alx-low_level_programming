8aeee6537f284be90b16ee675d863ce7a409a03c
awinbern <awinpangakudbillabernice4@gmail.com>
2022-04-08 14:14:28 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio-h>
+
+/**
+ * main - starting point
+ * Return: 0 means success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
