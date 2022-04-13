e554e14cefa52a84658006536feda07354017fb6
awinbern <awinpangakudbillabernice4@gmail.com>
2022-04-13 01:47:02 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_alphabet - It prints every character of an alphabet
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"1-alphabet.c" [New File]                                     0,0-1         All
+
