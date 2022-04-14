75ee69ef90f77443ff0ee958709152c5756238e1
awinbern <awinpangakudbillabernice4@gmail.com>
2022-04-14 13:39:55 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - detects whether the function prints a positive or negative integer
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
