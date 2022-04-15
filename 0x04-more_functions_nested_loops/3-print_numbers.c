708b379fb836a3cd9a5cb305148f7cf451f5bed5
awinbern <awinpangakudbillabernice4@gmail.com>
2022-04-15 13:54:58 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,16 @@
 
 /**
  * print_numbers - prints out  numbers
+ * Return: Always 0 (success)
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9 ; i++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
