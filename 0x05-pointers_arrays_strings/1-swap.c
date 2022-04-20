22f8d1429b4af69e540c9afa78b0d8f6a1c44105
awinbern <awinpangakudbillabernice4@gmail.com>
2022-04-20 04:53:48 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * swap_int - swap values
- * @a - pointer 1
- * @b - pointer 2
+ * @a: pointer 1
+ * @b: pointer 2
  * Return: void means succes
  */
 void swap_int(int *a, int *b)
