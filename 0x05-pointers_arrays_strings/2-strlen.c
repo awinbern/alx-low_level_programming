42d7be10e9b091b3ad73e65e8a3c03245fd4a870
awinbern <awinpangakudbillabernice4@gmail.com>
2022-04-20 05:04:30 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - length of string
+ * @s: pointer to the string
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
