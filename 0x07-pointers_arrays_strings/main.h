d0508d1c0008895d80b864d63bdea63f6e99a217
awinbern <awinpangakudbillabernice4@gmail.com>
2022-04-26 14:35:15 -0700

    Task 8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
