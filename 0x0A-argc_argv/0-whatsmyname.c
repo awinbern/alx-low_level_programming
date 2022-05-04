77bbf8cc43da1a25a84fb2ee445ae6c266c3daa1
awinbern <awinpangakudbillabernice4@gmail.com>
2022-05-04 14:46:55 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: parameter counter
+ * @argv: parameter pointer
+ * Return: int
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
